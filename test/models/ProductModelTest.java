4d8a284a4478f214cf4723b71882f9d90d49d07c
Wira Siwananda <siwananda@gmail.com>
2014-11-23 02:04:39 +0800

    activate some tests and remove some

--- test/models/ProductModelTest.java
+++ test/models/ProductModelTest.java
@@ -1,10 +1,12 @@
 package models;
 
 import org.junit.Test;
+import org.mongodb.morphia.Key;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ProductModelTest {
 
@@ -12,27 +14,29 @@ public class ProductModelTest {
     public void testCreate() throws Exception {
         ProductModel newProduct = new ProductModel();
         newProduct.setTitle("woohoo");
-        newProduct.getPricing().setPrice(1.20);
-        newProduct.getPricing().setCost(1.00);
-        ProductModel.create(newProduct);
+        PricingModel newPricing = new PricingModel();
+        newPricing.setPrice(1.20);
+        newPricing.setCost(1.00);
+        newProduct.setPricing(newPricing);
+        Key newProductKey = ProductModel.create(newProduct);
+        assertTrue(newProductKey.getId() != null);
     }
 
-    //
-//    public void testRemove() throws Exception {
-//
-//    }
-//
     @Test
     public void testListProduct() throws Exception {
         List<ProductModel> productModelList = ProductModel.list(0, 10);
         assertEquals(productModelList.size(), 10);
     }
-//
-//    public void testFindById() throws Exception {
-//
-//    }
-//
-//    public void testFindByName() throws Exception {
-//
-//    }
+
+    @Test
+    public void testFindById() throws Exception {
+        ProductModel productModel = ProductModel.findByProductId(9653);
+        assertTrue(9653 == productModel.getId());
+    }
+
+    @Test
+    public void testFindByQuery() throws Exception {
+        List<ProductModel> productModelList = ProductModel.listByQuery("/^" + 96 + ".*/.test(this.id)", 0, 10);
+        assertTrue(productModelList.size()>0);
+    }
 }
